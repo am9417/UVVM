cd759eb97fb4af8c29f68a28bdc3eb0dd308ad2d
Konsta M <konsta.maenpanen@outlook.com>
2025-12-19 10:18:15 +0200

    Run the uart vvd demo

---
+++ xsim/compile_uart_demo.ps1
@@ -0,0 +1,19 @@
+# Run this script in this xsim subfolder
+# xvhdl, xelab and xsim should be in the path
+
+# Compile dependencies
+.\compile_xsim.ps1 uvvm_util
+.\compile_xsim.ps1 uvvm_vvc_framework
+.\compile_xsim.ps1 bitvis_vip_scoreboard
+.\compile_xsim.ps1 bitvis_vip_sbi
+.\compile_xsim.ps1 bitvis_vip_uart
+.\compile_xsim.ps1 bitvis_vip_clock_generator
+.\compile_xsim.ps1 bitvis_uart
+
+# Compile demo TB
+xvhdl --2019 -work bitvis_uart ..\bitvis_uart\tb\uart_vvc_demo_th.vhd
+xvhdl --2019 -work bitvis_uart ..\bitvis_uart\tb\uart_vvc_demo_tb.vhd
+
+# Elaborate and run
+xelab bitvis_uart.uart_vvc_demo_tb
+xsim --R bitvis_uart.uart_vvc_demo_tb
